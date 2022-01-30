9a894713aff9633797e8de5996969948a39b8618
Kenta Oshima <84490675+kenteroshima@users.noreply.github.com>
2022-01-30 17:28:26 +0900

    Update macsetup.sh
    add Homebrew installation script

--- macsetup.sh
+++ macsetup.sh
@@ -2,6 +2,7 @@ sudo mv -f Applications .Applications
 sudo mv -f Movies .Movies
 sudo mv -f Public .Public
 
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 pip install django django-tailwind gym numpy matplotlib
