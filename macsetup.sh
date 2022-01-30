455287acf710eb97af33dfeaf247ed48d156ebc9
Kenta Oshima <84490675+kenteroshima@users.noreply.github.com>
2022-01-30 17:26:18 +0900

    Update macsetup.sh

--- macsetup.sh
+++ macsetup.sh
@@ -1,3 +1,7 @@
+sudo mv -f Applications .Applications                                           
+sudo mv -f Movies .Movies
+sudo mv -f Public .Public
+
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 pip install django django-tailwind gym numpy matplotlib
