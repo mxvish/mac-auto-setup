ca084aa19511b2b69a4ae84471429c22ca144985
Kenta Oshima <84490675+kenteroshima@users.noreply.github.com>
2022-03-22 19:57:53 +0900

    Update macsetup.sh

--- macsetup.sh
+++ macsetup.sh
@@ -1,6 +1,6 @@
-sudo mv -f Applications .Applications                                           
-sudo mv -f Movies .Movies
-sudo mv -f Public .Public
+sudo mv -f Applications ~/.Applications                                           
+sudo mv -f Movies ~/.Movies
+sudo mv -f Public ~/.Public
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
@@ -8,13 +8,15 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.c
 brew install lame
 brew install mpg321
 brew install mplayer
+brew install r
 
 pip install django
 pip install django-tailwind
 pip install gym
-pip install numpy
 pip install matplotlib
+pip install numpy
+pip install torch torchvision sklearn
 
-mv Rprofile .Rprofile
-mv vimrc .vimrc
-mv zshrc .zshrc
+mv Rprofile ~/.Rprofile
+mv vimrc ~/.vimrc
+mv zshrc ~/.zshrc
