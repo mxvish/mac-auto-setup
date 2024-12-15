d2a6521c3d283a75b56fec5389b74753431fb726
Kenta Oshima <84490675+mxvish@users.noreply.github.com>
2024-12-15 14:30:31 +0900

    don't install some apps

--- macsetup.sh
+++ macsetup.sh
@@ -4,7 +4,7 @@ sudo mv -f Downloads downloads
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-brew install google-japanese-ime memory-cleaner python-tk r ranger rstudio wget zoom
+brew install google-japanese-ime python-tk r ranger wget
 #brew link mysql-client --force
 
 """
